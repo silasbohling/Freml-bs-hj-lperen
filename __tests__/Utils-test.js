d9cc087e8cdab256535ca61eae2c440e2e5afcec
Silas Bøhling <>
2018-11-16 00:41:26 +0100

    Setup test enviroment

--- __tests__/Utils-test.js
+++ __tests__/Utils-test.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Utils from '@modules/Utils';
 
-it('should calculate correct setting', () => {
-  expect(5 + 4).toBe(9);
+describe('calculateSetting utility', () => {
+
+  it('should calculate correct setting', () => {
+    expect(Utils.calculateSetting(10)).toBeCloseTo(3.3);
+    expect(Utils.calculateSetting(6.9)).toBeCloseTo(3.4);
+  })
+
+})
+
+describe('formatUnixTimestamp utility', () => {
+
+  it('should display right date', () => {
+    const timestamp = "1542412800";
+    const format = "D/M HH:mm"
+    expect(Utils.formatUnixTimestamp(timestamp,format)).toBe("17/11 00:00");
+  })
+
 })
